3be64cf6a030df82505c06bfedc6249997315356
Amanda Martins Xavier <hi@amandamartins.dev>
2023-11-09 14:18:45 -0300

    :chart_with_upwards_trend: Feat: Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import App from "./app/App.vue";
 
 import { Inkline, components } from "@inkline/inkline";
 import "@inkline/inkline/inkline.scss";
+import { inject } from "@vercel/analytics";
 
 import "./styles/global.scss";
 import "./styles/style.scss";
 import router from "./app/router/router";
 
 const app = createApp(App);
+
+inject();
 
 app.use(Inkline, {
   colorMode: "light",
